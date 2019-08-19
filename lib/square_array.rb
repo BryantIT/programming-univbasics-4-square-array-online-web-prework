a1429a967ff593d6df6d0742ec5186b4f982320e
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-19 23:26:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   
   new_array= []
   counter=0 
-  while counter < array.length do
+  while array [counter] < array.length do
     new_array << [counter]**
     counter+=1
   end
