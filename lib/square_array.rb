60827effb8e2706521869e5a9fbe98b5aacdc98a
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-19 22:35:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array= []
   counter=0 
   while counter < array.length do
-    puts array [counter].sqrt
+    new_array << array [counter].sqrt
     counter+=1 
   end
 end
