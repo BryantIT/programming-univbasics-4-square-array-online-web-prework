50afef093ff3c05d145e18ed061826a1f868cbcf
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-19 23:42:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   
   new_array = []
   counter = 0 
-  while array [counter] do
+  while counter < array.length
     new_array << array[counter]**2
     counter += 1
-    return new_array
   end
 end
